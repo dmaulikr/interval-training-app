4e50be3c1a89f0665841f56e1cb67e6483beb8ee
J.P. Anderson-Young <jpayoung@live.ca>
2017-01-28 13:56:03 -0500

    Created project

---
+++ Interval-Training/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Interval-Training
+//
+//  Created by J.P. Anderson-Young on 2017-01-28.
+//  Copyright © 2017 J.P. Anderson-Young. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
